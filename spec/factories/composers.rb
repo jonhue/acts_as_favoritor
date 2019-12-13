bd3fc4414fa1899f0ba0b26330343735a521e4f7
Jonas Hübotter <me@jonhue.me>
2019-12-13 14:47:27 +0100

    address rubocop offenses

--- spec/factories/composers.rb
+++ spec/factories/composers.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :beethoven, class: Composer do |composer|
+  factory :beethoven, class: 'Composer' do |composer|
     composer.name { 'Beethoven' }
   end
 
-  factory :rossini, class: Composer do |composer|
+  factory :rossini, class: 'Composer' do |composer|
     composer.name { 'Rossini' }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :jon, class: User do |user|
+  factory :jon, class: 'User' do |user|
     user.name { 'Jon' }
   end
 
-  factory :sam, class: User do |user|
+  factory :sam, class: 'User' do |user|
     user.name { 'Sam' }
   end
 
-  factory :bob, class: User do |user|
+  factory :bob, class: 'User' do |user|
     user.name { 'Bob' }
   end
 end
