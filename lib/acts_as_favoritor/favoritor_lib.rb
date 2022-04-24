6a64842aebc43ee9a197a01250e46480eebcfa89
Jonas Hübotter <jonas.huebotter@gmail.com>
2022-04-24 12:26:37 +0200

    add rubocop-rails

--- lib/acts_as_favoritor/favoritor_lib.rb
+++ lib/acts_as_favoritor/favoritor_lib.rb
@@ -2,11 +2,11 @@
 
 module ActsAsFavoritor
   module FavoritorLib
-    def build_result_for_scopes(scopes)
+    def build_result_for_scopes(scopes, &block)
       return yield(scopes) unless scopes.is_a?(Array)
       return if scopes.empty?
 
-      sanitized_scopes(scopes).to_h { |scope| [scope, yield(scope)] }
+      sanitized_scopes(scopes).index_with(&block)
     end
 
     private
